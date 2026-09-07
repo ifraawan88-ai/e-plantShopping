e13ccf1f654957ded4b4a9b099df2fbcb098f3ae
ifraawan <ifra.awan.88@gmail.com>
2026-09-07 15:28:31 +0500

    Add Paradise Nursery landing page

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+
+        <p>
+          Welcome to Paradise Nursery, your online destination for
+          beautiful and healthy plants.
+        </p>
+
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
