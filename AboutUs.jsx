f415e0b057666a450353fdd6e7af0f8b89638d6a
ifraawan <ifra.awan.88@gmail.com>
2026-09-07 15:12:26 +0500

    Initial Paradise Nursery project

---
+++ AboutUs.jsx
@@ -0,0 +1,27 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant shop that provides a variety
+        of beautiful plants for homes, offices, and gardens.
+      </p>
+
+      <p>
+        Our goal is to make it easy for customers to explore plants,
+        view their details and prices, and add their favorite plants
+        to a shopping cart.
+      </p>
+
+      <h2>Our Mission</h2>
+
+      <p>
+        We aim to provide a simple and user-friendly online shopping
+        experience for plant lovers.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
